98dc2d836ec50f06eb98274ddb1651cfafa60209
Michael _377 <yelubin377@gmail.com>
2026-07-23 20:25:01 +0800

    修复了一些已知问题

--- muduo/Socket.h
+++ muduo/Socket.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "noncopyable.h"
+#include "InetAddress.h"
 
 class Socket : noncopyable
 {
@@ -7,38 +8,39 @@ public:
     explicit Socket(int sockfd) : sockfd_(sockfd){}
     ~Socket() { ::close(sockfd_);}
     int fd() const { return sockfd_; }
+                 
+    void bind(const InetAddress& addr)
+    {
+        ::bind(sockfd_, addr.getSocketAddr(), sizeof(addr));
+    }
+    void listen()
+    {
+        ::listen(sockfd_, SOMAXCONN);
+    }
+
+    typedef struct
+    {
+        int socket;
+        InetAddress peerAddr;
+    }Accept_Result;
+
+    Accept_Result accept()
+    {
+        sockaddr_in addr;
+        socklen_t addrlen = sizeof(addr);
+        int connfd = ::accept4(sockfd_, (sockaddr*)&addr, &addrlen,
+                               SOCK_NONBLOCK | SOCK_CLOEXEC);
+        if(connfd < 0)
+        {
+            perror("accept");
+            return(-1,InetAddress());
+        }
+        return {connfd, InetAddress(addr)};
+    }
+
 private:
     const int sockfd_;
 };
-
-void bind(const InetAddress& addr)
-{
-    ::bind(sockfd_,addr.getSocketAddr(),sizeof(addr));
-}
-void listen()
-{
-    ::listen(sockf_,SOMAXCONN);
-}
-
-typedef struct AcceptResult
-{
-    int socket;
-    InetAddress& peerAddr;
-}result(Accept);
-
-Socket::result(Accept) accept()
-{
-    sockaddr_in addr;
-    socklen_t addrlen = sizeof(addr);
-    int connfd = ::accept4(sockfd_,(sockaddr*)&addr,&addrlen,
-                           SOCK_NONBLOCK | SOCK_CLOEXEC);
-    if(connfd < 0)
-    {
-        perror("accept");
-    }
-    return {connfd,InetAddress(addr)};
-}
-
 
 
 
