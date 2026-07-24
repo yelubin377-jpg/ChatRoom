d824a0a6deb6282e91aeb8a58db58c89e671a378
Michael _377 <yelubin377@gmail.com>
2026-07-24 20:25:01 +0800

    修复了一些已知问题

--- muduo/noncopyable.h
+++ muduo/noncopyable.h
@@ -1,9 +1,12 @@
 #pragma once
 class noncopyable
 {
-protected:   //只有自己和自己的子类可以访问
+protected:           //只有自己和自己的子类可以访问， 不能创建如：class EventLoop  :  noncopyable 
     noncopyable() = default;
     ~noncopyable() = default;
-    noncopyable( const noncopyable& ) = delete;
-    const noncopyable& operator=( const noncopyable& ) = delete;
-};
+    
+    noncopyable(const noncopyable&) = delete;//不存在noncopyable(const noncopyable&)
+    //上面是拷贝构造，下面是拷贝赋值 
+    void noncopyable& operator = (noncopyable&) = delete;
+}
+
